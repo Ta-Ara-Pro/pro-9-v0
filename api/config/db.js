412a2f5e86265a9d4ae36f86d620954fba36b899
Ta-Ara-Pro <tahereh.q.ara@gmail.com>
2025-01-18 18:44:33 +0000

    first commit

---
+++ api/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI
+			,{ serverSelectionTimeoutMS: 30000,}
+		);
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.error(`MongoDB connection Error: ${error.message}`);
+		process.exit(1); // process code 1 code means exit with failure, 0 means success
+	}
+};
---
+++ api/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// Middleware
+if (process.env.NODE_ENV === 'production') {
+	app.use(cors({
+	  origin: 'https://yourfrontenddomain.com',
+	  credentials: true,
+	}));
+  } else {
+	app.use(cors({ origin: "http://localhost:5173" }));
+  }
+  
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
+//===================================
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+});
+
+//----------------------------------
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
+});
